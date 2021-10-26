b3c5bfcc661d0681047fb91b679a40bd0ecba0fa
Guilherme Lellis <lguilherme44@gmail.com>
2021-10-25 23:53:46 -0300

    frontend

---
+++ frontend/src/contexts/auth.tsx
@@ -0,0 +1,88 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+type AuthProver = {
+  children: ReactNode;
+};
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+
+export function AuthProvider({ children }: AuthProver) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const signInUrl = `${import.meta.env.VITE_SIGNIN_URL}${
+    import.meta.env.VITE_CLIENT_ID
+  }`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:token", token);
+
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+    setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem("@dowhile:token");
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@dowhile:token");
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+      api.get<User>("profile").then((response) => {
+        setUser(response.data);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWihoutCode, githubCode] = url.split("?code=");
+
+      window.history.pushState({}, "", urlWihoutCode);
+
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
